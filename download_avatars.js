f5500a0d37fcf4b8d5320653ca084831acbc995c
sevenveils <hghamilton@gmail.com>
2017-05-09 02:05:21 +0000

    step 5 commit

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,25 @@
+var GITHUB_USER = "sevenveils";
+var GITHUB_TOKEN = "6e2637ddad31997de7e89b662b64f8954620e75f";
+
 var request = require('request');
 
+
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+  url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+  headers: {
+    'User-Agent': 'sevenveils'
+  }
+};
+  request(options, cb);
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, response, body) {
+  let parsedResults = JSON.parse(body);
+  console.log(parsedResults);
+  console.log("Errors:", err);
+  console.log("Result:", parsedResults);
+});
