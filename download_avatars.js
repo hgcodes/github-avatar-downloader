a8dadd249720bf71c9b940b44b8c404697e06480
sevenveils <hghamilton@gmail.com>
2017-05-09 02:57:03 +0000

    step 10 commit

--- download_avatars.js
+++ download_avatars.js
@@ -2,7 +2,7 @@ var GITHUB_USER = "sevenveils";
 var GITHUB_TOKEN = "6e2637ddad31997de7e89b662b64f8954620e75f";
 
 var request = require('request');
-
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -17,12 +17,17 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 }
 
-getRepoContributors("jquery", "jquery", function(err, response, body) {
+getRepoContributors(process.argv[2], process.argv[3], function(err, response, body) {
   let parsedResults = JSON.parse(body);
   for (var i = 0; i < parsedResults.length; i++) {
     console.log(parsedResults[i]["avatar_url"]);
+    downloadImageByURL(parsedResults[i]["avatar_url"], `avatars/${i}.jpg`)
   }
   // console.log(parsedResults);
   // console.log("Errors:", err);
   // console.log("Result:", parsedResults);
 });
+  function downloadImageByURL(url, filePath) {
+  request(url).pipe(fs.createWriteStream(filePath))
+}
+
