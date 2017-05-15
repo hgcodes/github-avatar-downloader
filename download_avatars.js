0ded4657b6a95e69e601f611da08e00d51977367
Heather Hamilton <HG@HG.local>
2017-05-14 18:37:27 -0700

    Avatar Download project modified to include name of avatar owner when downloaded

--- download_avatars.js
+++ download_avatars.js
@@ -7,27 +7,35 @@ var fs = require('fs');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-  url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
-  headers: {
-    'User-Agent': 'sevenveils'
-  }
-};
-  request(options, cb);
+  if (!repoOwner || !repoName){
+    console.log("ERROR! You did not enter a valid repo owner or repo name.");
+  } else {
+    var options =  {
+      url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+      headers: {
+        'User-Agent': 'sevenveils'
+      }
+    };
 
+    request(options, cb);
+  }
 }
 
-getRepoContributors(process.argv[2], process.argv[3], function(err, response, body) {
-  let parsedResults = JSON.parse(body);
-  for (var i = 0; i < parsedResults.length; i++) {
-    console.log(parsedResults[i]["avatar_url"]);
-    downloadImageByURL(parsedResults[i]["avatar_url"], `avatars/${i}.jpg`)
+function downloadingImageByURL(url, filepath) {
+  console.log('Downloading avatar', url, 'to:', filepath);
+  request(url).pipe(fs.createWriteStream(filepath));
+}
+getRepoContributors(process.argv[2], process.argv[3], function(err, result, body) {
+  if (err || result.statusCode !== 200) {
+    console.log('Error retrieving avatars:', err);
+    console.log('HTTP status code:', result.statusCode);
+    console.log('Error message:', result.statusMessage);
+  } else {
+    let parsedResults = JSON.parse(body);
+    for (var i = 0 ; i < parsedResults.length; i++) {
+      let entry = parsedResults[i];
+      let avatarUrl = entry['avatar_url'];
+      downloadingImageByURL(avatarUrl, `avatars/${entry.login}.jpg`);
+    }
   }
-  // console.log(parsedResults);
-  // console.log("Errors:", err);
-  // console.log("Result:", parsedResults);
 });
-  function downloadImageByURL(url, filePath) {
-  request(url).pipe(fs.createWriteStream(filePath))
-}
-
